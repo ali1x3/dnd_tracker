6e52e9423a871008d0e6c1f9fa186be292e087b6
ali1x3 <avilaalex371@gmail.com>
2025-11-17 09:26:03 +0800

    not sure

--- src/class.rs
+++ src/class.rs
@@ -1,24 +1,52 @@
-use iced::widget::{button, text};
+use iced::widget::{button, column, text};
 use iced::{Element, Task};
 
 
 #[derive(Debug, Default)]
 pub struct Class {
+    state: State,
+}
+
+#[derive(Debug)]
+pub enum State {
+    Display,
+    Edit,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::Display
+    }
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     Display,
-    Back,
+    Edit,
 }
 
 impl Class{
+
     pub fn update(&mut self, message: Message) -> Task<Message> {
-        Task::none()
+        let thing = match message {
+            Message::Display => self.state = State::Display,
+            Message::Edit => self.state = State::Edit,
+        }.into();
+        thing
     }
 
     pub fn view(&self) -> Element<'_, Message> {
-        text("class").into()
+
+        let content = match &self.state {
+            State::Edit => text("This is editing class"),
+            State::Display => text("this is class"),
+        };
+
+        column![
+            content,
+            button(text("edit")).on_press(Message::Edit),
+            button(text("display")).on_press(Message::Display),
+        ].into()
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use iced::widget::{button, column, row, text, Row};
-use iced::{Element, Font, Task, Theme, window};
+use iced::widget::{button, center, column, container, row, text, Space};
+use iced::Length::Fill;
+use iced::{border, window, Element, Font, Length, Padding, Task, Theme};
 
 mod class;
 
@@ -7,33 +8,70 @@ mod class;
 #[derive(Default)]
 struct App {
     class: class::Class,
+    current_screen: Screen,
+}
+
+enum Screen {
+    Class,
+    Home,
+}
+
+impl Default for Screen {
+    fn default() -> Self {
+        Screen::Home
+    }
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     Class(class::Message),
+    Home,
     Exit,
 }
 
 impl App {
-
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::Exit => window::get_latest().and_then(window::close),
-            Message::Class(msg) => self.class.update(msg).map(Message::Class),
+            Message::Class(msg) => {
+                self.current_screen = Screen::Class;
+                self.class.update(msg).map(Message::Class)
+            },
+            Message::Home => {
+                self.current_screen = Screen::Home;
+                Task::none()
+            },
         }
     }
 
     fn view(&self) -> Element<'_, Message> {
-        let header = row![
-            text("app").size(20).font(Font::MONOSPACE),
-            button(text("Exit")).on_press(Message::Exit),
-        ];
+        let header = container(row![
+            button(text("Characters")).on_press(Message::Home).width(Fill).height(Fill),
+            button(text("Races")).on_press(Message::Home).width(Fill).height(Fill),
+            button(text("Classes")).on_press(Message::Class(class::Message::Display)).width(Fill).height(Fill),
+            button(text("Spells")).on_press(Message::Class(class::Message::Display)).width(Fill).height(Fill),
+            button(text("Items")).on_press(Message::Class(class::Message::Display)).width(Fill).height(Fill),
+            button(text("Backgrounds")).on_press(Message::Class(class::Message::Display)).width(Fill).height(Fill),
+            button(text("Home")).on_press(Message::Home).width(Fill).height(Fill),
+            button(text("Exit")).on_press(Message::Exit).width(Fill).height(Fill),
+        ]).height(40)
+        .padding(Padding {top: 0.0, right: 0.0, bottom: 10.0, left: 0.0} );
 
+        let contents = match self.current_screen {
+            Screen::Home => text("this is home").into(),
+            Screen::Class => self.class.view().map(Message::Class),
+        };
 
         column![
             header,
-            button(text("Classes")).on_press(Message::Class(class::Message::Display)),
+            center(contents)
+                .style(|theme| {
+                    let palette = theme.extended_palette();
+
+                    container::Style::default()
+                        .border(border::color(palette.background.strong.color).width(4))
+                })
+                .padding(20)
         ].into()
     }
 
